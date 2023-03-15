f115b33def8e560749cea6e713bc33d1375c730e
apers <apers@student.chalmers.se>
2023-03-15 14:11:00 +0100

    lolssssss

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { Configuration, OpenAIApi } from "openai";
 import openAiRoutes from "./routes/openai.js";
 import authRoutes from "./routes/auth.js";
 
-const path = require("path");
+const path = require('node:path');
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
